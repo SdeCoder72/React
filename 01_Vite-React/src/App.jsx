b389281d987f99b8f4675dd9606084cb37fb69c3
CodeDev <153368092+SdeCoder72@users.noreply.github.com>
2025-05-10 12:36:36 +0530

    created my first component with React + Vite

---
+++ 01_Vite-React/src/App.jsx
@@ -0,0 +1,10 @@
+import First from "./first"
+
+function App() {
+
+  return (
+    <First />
+  )
+}
+
+export default App
---
+++ 01_Vite-React/src/first.jsx
@@ -0,0 +1,11 @@
+
+function First() {
+    return(
+        <>
+        <h1>Hello React + Vite!</h1>
+        <h2>This is my first project of React with Vite.</h2>
+        </>
+    )
+}
+
+export default First
